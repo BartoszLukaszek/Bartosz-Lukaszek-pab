ce5cdd3f64b46b64ad1327043c4f6ad6fbdbde75
BartoszLukaszek <barti120@onet.pl>
2022-04-20 10:59:31 +0200

    index

--- lab2/src/index.ts
+++ lab2/src/index.ts
@@ -1,16 +1,47 @@
-import express from 'express'
+import express, { json } from 'express'
 import {Request, Response} from 'express'
 
-const app = express()
+var app = express();
 
 app.use(express.json())
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
-})
-app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  res.sendStatus(200).send('POST Hello World')
-})
+const note = require("./note/note");
+app.use("/note",note)
 
-app.listen(3000)
+// app.post('/tag', function (req: Request, res: Response) {
+//     const name:string = req.body.name
+//     const id:number = req.body.id
+//     let exist = false
+//     tags.forEach(element => {
+//         if(element.name.toLowerCase() === name.toLowerCase()){
+//             exist = true
+//             return
+//         }
+//     });
+//     if(exist){
+//         res.status(400).send()
+//     }else{
+//         const tag = new Tag(name,id)
+//         tags.push(tag)
+//         res.status(200).send()
+//     }
+
+// });
+
+
+
+// app.get('/tags', function (req: Request, res: Response) {
+//     if(tags.length >0 ){
+//         res.status(200).send(tags)
+//     }else{
+//         res.status(400).send("brak")
+//     }
+// });
+
+
+const login = require("./login/login");
+app.use("/login",login)
+
+
+
+app.listen(3001,() => console.log("Uruchomiona na porcie 3001"))
